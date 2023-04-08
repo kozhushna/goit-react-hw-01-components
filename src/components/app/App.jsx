d182f342681051e28ba5322ada43ed0176ebbddc
kozhushna <k.kozhushna@gmail.com>
2023-04-08 13:23:06 +0300

    Add transactions

---
+++ src/components/app/App.jsx
@@ -0,0 +1,20 @@
+import Profile from '../profile/Profile';
+import Statistics from '../statistics/Statistics';
+import FriendList from '../friendList/FriendList';
+import TransactionHistory from '../transactionHistory/TransactionHistory';
+
+import user from '../../source/user.json';
+import data from '../../source/data.json';
+import friends from '../../source/friends.json';
+import transactions from '../../source/transactions.json';
+
+export const App = () => {
+  return (
+    <div className="app-container">
+      <Profile {...user} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </div>
+  );
+};
