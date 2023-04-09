250944f09439b8f3aa8da78be2774a0cd1b903ce
kozhushna <k.kozhushna@gmail.com>
2023-04-09 17:56:49 +0300

    Add colors stats

--- src/components/statisticsItem/StatisticsItem.jsx
+++ src/components/statisticsItem/StatisticsItem.jsx
@@ -2,8 +2,23 @@ import PropTypes from 'prop-types';
 import './StatisticsItem.css';
 
 const StatisticsItem = ({ label, percentage }) => {
+  const bgColor = percentage => {
+    let color = '';
+    if (percentage < 20) {
+      color = 'rgb(152, 252, 190)';
+    } else if (percentage >= 20 && percentage < 40) {
+      color = 'rgb(132, 188, 241)';
+    } else if (percentage >= 40 && percentage < 60) {
+      color = 'rgb(229, 189, 245)';
+    } else if (percentage >= 60 && percentage < 80) {
+      color = 'rgb(252, 197, 218)';
+    } else {
+      color = 'rgb(248, 247, 167)';
+    }
+    return { background: color };
+  };
   return (
-    <li className="item">
+    <li className="item" style={bgColor(percentage)}>
       <span className="label">{label}</span>
       <span className="percentage">{percentage}%</span>
     </li>
